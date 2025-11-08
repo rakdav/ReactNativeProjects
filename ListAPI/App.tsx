53ac9adef08b094704f88209d8430faf1baa80d9
rakdav <rakdav@inbox.ru>
2025-11-08 14:43:59 +0200

    first

---
+++ ListAPI/App.tsx
@@ -0,0 +1,45 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {useEffect, useState} from "react";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "https://jsonplaceholder.typicode.com/posts"
+  useEffect(() => {
+    fetch(url)
+        .then((resp) => resp.json())
+        .then((json) => setData(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+  }, [])
+  return (
+      <View style={styles.container}>
+        {loading ? (
+            <Text>Loading...</Text>
+        ) : (
+            data.map((post) => {
+              return (
+                  <View>
+                    <Text style={styles.title}>{post.title}</Text>
+                    <Text>{post.body}</Text>
+                  </View>
+              );
+            })
+        )}
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+});
