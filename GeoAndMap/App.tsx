97f12c9932085d134fe2e09b270fbf2ae2c292e1
rakdav <rakdav@inbox.ru>
2025-11-15 13:15:03 +0200

    1

---
+++ GeoAndMap/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import MapView,{Marker} from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+
+Sta
+export default function App() {
+    return (
+
+        // <View style={styles.container}>
+        //     <MapView style={styles.map}
+        //     showsPointsOfInterest={false}
+        //     showsUserLocation
+        //     followsUserLocation>
+        //         <Marker title="Тесов место"
+        //                 description="Не приближаться"
+        //                 coordinate={{
+        //                     latitude:54.43,
+        //                     longitude:20.453
+        //                 }}
+        //         />
+        //     </MapView>
+        // </View>
+
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor:"ghostwhite"
+    },
+    mapView: {
+        alignSelf:"stretch",
+        width:450,
+        height:30,
+    },
+});
